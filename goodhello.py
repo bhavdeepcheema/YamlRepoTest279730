f49232bb59f290efd4c6683b02ef9237083dfffd
bhavdeepcheema <bhavdeep@academic.rrc.ca>
2026-01-20 12:32:12 -0600

    new files added

---
+++ goodhello.py
@@ -0,0 +1,15 @@
+def hello():
+    """
+    Print a simple greeting.
+    """
+    print("Hello, DevOps Learner!")
+
+def bye():
+    """
+    Print a goodbye message.
+    """
+    print("bye, DevOps Learner!")
+
+if __name__ == "__main__":
+    hello()
+    bye()
